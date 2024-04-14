1bd11d04e73ce6cd69cc68920714340dea1a05d5
MHK <135600102+DB0R@users.noreply.github.com>
2024-04-14 08:35:55 +0200

    Update ee.js
    Signed-off-by: MHK <135600102+DB0R@users.noreply.github.com>

--- ee.js
+++ ee.js
@@ -1,3 +1,3 @@
 document.getElementById("downloadBtn").addEventListener("click", function() {
-    window.location.href = "https://www.mediafire.com/file/3nt7o3h7sz6l9tl/%D8%A7%D9%84%D9%82%D9%84%D8%B9%D9%87+%D8%A7%D9%84%D8%AD%D9%85%D8%B1%D8%A7%D8%A1.apk/file?dkey=hydd0fxivbb&r=1634/file/path/to/your/file.apk/file/path/to/your/file.apk/path/to/your/file.zip'";
+    window.location.href = "https://https://www.mediafire.com/file/7pq1yo08t5dhj6c/القلعه+الحمراء.apk/file/file/path/to/your/file.apk/path/to/your/file.zip'";
 });
