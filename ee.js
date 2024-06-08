bec7b75a2b625bf53e9bf9c4e72ccfdfaadc80f6
MHK <135600102+DB0R@users.noreply.github.com>
2024-06-09 00:29:01 +0300

    Update ee.js
    Signed-off-by: MHK <135600102+DB0R@users.noreply.github.com>

--- ee.js
+++ ee.js
@@ -1,3 +1,3 @@
 document.getElementById("downloadBtn").addEventListener("click", function() {
-    window.location.href = "https://www.mediafire.com/file/5llo8hziuwivt6o/%25D8%25A7%25D9%2584%25D9%2582%25D9%2584%25D8%25B9%25D9%2587_%25D8%25A7%25D9%2584%25D8%25AD%25D9%2585%25D8%25B1%25D8%25A7%25D8%25A1.apk/file";
+    window.location.href = "https://www.mediafire.com/file/vvtgsrlqb8emjgu/%25D8%25A7%25D9%2584%25D9%2582%25D9%2584%25D8%25B9%25D9%2587_%25D8%25A7%25D9%2584%25D8%25AD%25D9%2585%25D8%25B1%25D8%25A7%25D8%25A1.apk/file";
 });
