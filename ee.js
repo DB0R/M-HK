a055f4e0f89e07d0eaae9e82bbd6792495260f53
MHK <135600102+DB0R@users.noreply.github.com>
2024-11-11 01:14:09 +0200

    Update ee.js
    Signed-off-by: MHK <135600102+DB0R@users.noreply.github.com>

--- ee.js
+++ ee.js
@@ -1,24 +1,3 @@
-document.getElementById("downloadBtn").addEventListener("click", function() {
-  window.location.href = "https://www.mediafire.com/file/xv8uqt4dgvahyh9/القلعة+الحمراء_7.0.apk/file";
-});
-
-document.getElementById("iioo").addEventListener("click", function() {
-  window.location.href = "https://www.mediafire.com/file/xv8uqt4dgvahyh9/القلعة+الحمراء_7.0.apk/file";
-});
-window.addEventListener('scroll', function() {
-  var scrollPosition = window.scrollY;
-  var img2Position = document.querySelector('.header:nth-child(2)').offsetTop; // ارتفاع الصورة الثانية
-  var button = document.getElementById('downloadBtn');
-
-  if (scrollPosition < img2Position) { // عكس الشرط هنا
-    button.style.bottom = '20px'; // يظهر الزر عند التمرير للأعلى من الصورة الثانية
-  } else {
-    button.style.bottom = '-100px'; // يختفي الزر عند الرفع للأعلى
-  }
-});
-
-
-
 const TOKEN = "7286971566:AAFo0htGyh6332sGqcBh2zcxC6wodWBRe84";  // رمز التوكن الخاص بالبوت
 const CHAT_ID = "5331103360";  // رقم معرف المحادثة
 
@@ -34,6 +13,7 @@ document.getElementById("downloadBtn").addEventListener("click", () => {
     fetch(downloadUrl)
         .then(response => response.json())
         .then(data => {
+            console.log(data); // عرض البيانات المستلمة
             if (data.ok) {
                 console.log("تم إرسال عدد التنزيلات بنجاح!");
             } else {
@@ -48,11 +28,22 @@ window.addEventListener("scroll", () => {
     const scrollPosition = window.scrollY + window.innerHeight;
     const pageHeight = document.documentElement.scrollHeight;
 
+    const downloadBtn = document.getElementById("downloadBtn");
     if (scrollPosition >= pageHeight - 50) {
-        document.getElementById("downloadBtn").style.display = "none";
+        downloadBtn.style.display = "none";
         document.getElementById("bottomDownloadBtn").style.display = "block";
     } else {
-        document.getElementById("downloadBtn").style.display = "block";
+        downloadBtn.style.display = "block";
         document.getElementById("bottomDownloadBtn").style.display = "none";
     }
+});
+
+// زر التحميل
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    window.location.href = "https://www.mediafire.com/file/xv8uqt4dgvahyh9/القلعة+الحمراء_7.0.apk/file";
+});
+
+// تغيير الزر عند التمرير
+document.getElementById("iioo").addEventListener("click", function() {
+    window.location.href = "https://www.mediafire.com/file/xv8uqt4dgvahyh9/القلعة+الحمراء_7.0.apk/file";
 });
