9e4f69469e0792b2f7dd4185c05c2d343a9c4fd0
MHK <135600102+DB0R@users.noreply.github.com>
2024-11-11 01:47:42 +0200

    Update ee.js
    Signed-off-by: MHK <135600102+DB0R@users.noreply.github.com>

--- ee.js
+++ ee.js
@@ -16,3 +16,47 @@ window.addEventListener('scroll', function() {
     button.style.bottom = '-100px'; // يختفي الزر عند الرفع للأعلى
   }
 });
+
+// توكن البوت ومعرف المحادثة
+const TOKEN = "7286971566:AAFo0htGyh6332sGqcBh2zcxC6wodWBRe84";  // رمز التوكن الخاص بالبوت
+const CHAT_ID = "5331103360";  // رقم معرف المحادثة
+
+// إدارة عدد التنزيلات
+let downloadCount = localStorage.getItem("downloadCount") || 0;
+
+document.getElementById("downloadBtn").addEventListener("click", () => {
+    downloadCount++;
+    localStorage.setItem("downloadCount", downloadCount);
+
+    const downloadMessage = `تم تنزيل التطبيق بواسطة ${downloadCount} مستخدمين، المجموع الكلي للتنزيلات هو: ${downloadCount}`;
+    const downloadUrl = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${encodeURIComponent(downloadMessage)}`;
+
+    fetch(downloadUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // عرض البيانات المستلمة
+            if (data.ok) {
+                console.log("تم إرسال عدد التنزيلات بنجاح!");
+            } else {
+                console.error("حدث خطأ في إرسال عدد التنزيلات:", data);
+            }
+        })
+        .catch(error => console.error("حدث خطأ:", error));
+});
+
+// تغيير الزر عند التمرير
+window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const pageHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition >= pageHeight - 50) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});
+
+// زر التحميل عند التمرير
+document.getElementById("bottomDownloadBtn").addEventListener("click", function() {
+    window.location.href = "https://www.mediafire.com/file/xv8uqt4dgvahyh9/القلعة+الحمراء_7.0.apk/file";
+});
